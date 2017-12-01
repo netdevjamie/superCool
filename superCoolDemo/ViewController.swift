0bb2927c8184eac970bc34c33399840ba426f81c
jwm5909 <jmclaughlin@jjkeller.com>
2017-12-01 14:15:02 -0500

    init commit

---
+++ superCoolDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  superCoolDemo
+//
+//  Created by Jamie Mclaughlin on 12/1/17.
+//  Copyright © 2017 Jamie Mclaughlin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
